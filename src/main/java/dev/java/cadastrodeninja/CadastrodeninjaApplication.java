a5fe0818ecdabf1a9e3bec3f6e6aeb691853ac89
Davi Veloso <davi.veloso@foton.la>
2024-12-04 16:59:37 -0300

    COmmit do projeto

---
+++ src/main/java/dev/java/cadastrodeninja/CadastrodeninjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java.cadastrodeninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeninjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeninjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/cadastrodeninja/CadastrodeninjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.cadastrodeninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeninjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
